612cfd381c043b37db166638fe8f2ceeb5baec60
Luiz Henrique <luizhenriqueprudenciosouza@gmail.com>
2021-09-09 15:58:27 -0300

    Create exercicio-05.py

---
+++ exercicio-05.py
@@ -0,0 +1,8 @@
+lista = ['BANANA', 'MAÇA', 'LARANJA', 'MORANGO'] 
+
+l = input("Escolha o alimento que deseja verificar ")
+
+if l in lista:
+    print("Sim, está na lista!")
+else:
+    print("Não está na lista")
