98fbe16e34ef4cbb94814fcbf2b86762bcf82bc7
Luiz Henrique <luizhenriqueprudenciosouza@gmail.com>
2021-09-09 17:20:55 -0300

    Create exercicio-08.py

---
+++ exercicio-08.py
@@ -0,0 +1,11 @@
+compra = ['Mouse Logitech g403', 'Teclado Redragon Swich Blue']
+print(compra)
+print("Adicionando mais dois itens...")
+compra.append("Gabinete Cooler Master")
+compra.append("placa de video GeForce RTX 2080Ti")
+print (compra)
+print ("Valor fora do orçamento ... Removendo itens !!!")
+
+del compra[0]
+del compra[0]
+print(compra)
