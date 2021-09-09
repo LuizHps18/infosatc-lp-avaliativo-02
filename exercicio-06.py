792d8f9df77f397372709c4436b02647a6341d14
Luiz Henrique <luizhenriqueprudenciosouza@gmail.com>
2021-09-09 16:00:55 -0300

    Create exercicio-06.py

---
+++ exercicio-06.py
@@ -0,0 +1,5 @@
+lista = ['UVA', 'AMEIXA', 'PÊRA', 'ABACAXI', 'CARAMBOLA']
+
+l = input("Escolha o alimento que deseja verificar a posição ")
+
+print("Encontrando a posição de um item na lista: ", lista.index(l))
