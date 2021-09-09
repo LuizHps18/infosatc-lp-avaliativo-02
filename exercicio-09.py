2ca9db0c186eaa7f9d5ef88954a3a64c2020a780
Luiz Henrique <luizhenriqueprudenciosouza@gmail.com>
2021-09-09 17:20:57 -0300

    Create exercicio-09.py

---
+++ exercicio-09.py
@@ -0,0 +1,8 @@
+n1 = int(input("Digite um numero inteiro aleatorio          : "))
+n2 = int(input("Digite um numero inteiro aleatorio          : "))
+n3 = int(input("Digite um numero inteiro aleatorio          : "))
+n4 = int(input("Digite um numero inteiro aleatorio          : "))
+n5 = int(input("Digite um numero inteiro aleatorio          : "))
+
+lista = [n1, n2, n3, n4, n5]
+print("minha lista ordenada numérica: ", sorted(lista))
