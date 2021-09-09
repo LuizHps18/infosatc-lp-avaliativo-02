5003f2d37dfb434928b04ba9007a424cef55e5b5
Luiz Henrique <luizhenriqueprudenciosouza@gmail.com>
2021-09-09 15:43:44 -0300

    Create exercicio-03.py

---
+++ exercicio-03.py
@@ -0,0 +1,9 @@
+n1 = int(input("Digite o Primeiro numero inteiro           : "))
+n2 = int(input("Digite o Segundo numero inteiro            : "))
+n3 = int(input("Digite o Terceiro numero inteiro           : "))
+n4 = int(input("Digite o Quarto numero inteiro             : "))
+n5 = int(input("Digite o Quinto numero inteiro             : "))
+
+
+lista = [n1, n2, n3, n4, n5]
+print("A soma dos numeros inteiros é de:", sum(lista))
